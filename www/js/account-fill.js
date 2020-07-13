0e155cb290340190a2871e498dac9ce52fcda53d
svirom <svirom@yahoo.com>
2020-07-13 17:18:00 +0300

    my archive

--- www/js/account-fill.js
+++ www/js/account-fill.js
@@ -2,57 +2,58 @@ $(document).ready(function() {
 
   var docWidth = $(document).width();
 
-  var popover1 = `
-    <div class="popover-account popover-account--top active" id="popover-account-1">
+  var popoverSearch = `
+    <div class="popover-account popover-account--search active" id="popover-account-search">
       <p>Быстро ищите поставщиков услуг с помощью поиска по сайту</p>
       <a href="#" class="btn btn-outline btn-block">Понятно</a>
     </div>`;
 
-  var popover2 = `
-    <div class="popover-account popover-account--left" id="popover-account-2">
+  var popoverTemplates = `
+    <div class="popover-account popover-account--templates" id="popover-account-templates">
       <p>Здесь хранятся ваши шаблоны для быстрой оплаты. Создавайте новые и управляйте сушествующими!</p>
       <a href="#" class="btn btn-outline btn-block">Понятно</a>
     </div>`;
 
-  var popover3 = `
-    <div class="popover-account popover-account--left" id="popover-account-3">
+  var popoverPayments = `
+    <div class="popover-account popover-account--payments" id="popover-account-payments">
       <p>В архиве платежей вы найдете информацию о ваших платежах и все квитанции</p>
       <a href="#" class="btn btn-outline btn-block">Понятно</a>
     </div>`;
 
-  var popover4 = `
-    <div class="popover-account popover-account--left" id="popover-account-4">
+  var popoverMenu = `
+    <div class="popover-account popover-account--menu" id="popover-account-menu">
       <p>С помощью меню вы можете перейти в раздел мои карты и привязать карту к своему кошельку Masterpass для мгновенной оплаты услуг</p>
-      <a href="#" class="btn btn-outline btn-block">Понятно</a>
+      <a href="#" class="btn btn-outline btn-block">Завершить!</a>
     </div>`;
 
-  $('.account-main .form-search').prepend(popover1);
-  $('.account-templates h3').prepend(popover2);
-  $('.account-payments h3').prepend(popover3);
-  $('.catalogue-dashboard').prepend(popover4);
+  $('.account-main .form-search').prepend(popoverSearch);
+  $('.account-templates h3').prepend(popoverTemplates);
+  $('.account-payments h3').prepend(popoverPayments);
 
-  $('#popover-account-1 a').on('click', function(e) {
+  docWidth >= 992 ? $('.catalogue-dashboard').prepend(popoverMenu) : $('.main-nav__item-mobile').prepend(popoverMenu);
+
+  $('#popover-account-search a').on('click', function(e) {
     e.preventDefault();
 
     $(this).closest('.popover-account').removeClass('active');
-    $('#popover-account-2').addClass('active');
+    $('#popover-account-templates').addClass('active');
   })
 
-  $('#popover-account-2 a').on('click', function(e) {
+  $('#popover-account-templates a').on('click', function(e) {
     e.preventDefault();
 
     $(this).closest('.popover-account').removeClass('active');
-    $('#popover-account-3').addClass('active');
+    $('#popover-account-payments').addClass('active');
   })
 
-  $('#popover-account-3 a').on('click', function(e) {
+  $('#popover-account-payments a').on('click', function(e) {
     e.preventDefault();
 
     $(this).closest('.popover-account').removeClass('active');
-    $('#popover-account-4').addClass('active');
+    $('#popover-account-menu').addClass('active');
   })
 
-  $('#popover-account-4 a').on('click', function(e) {
+  $('#popover-account-menu a').on('click', function(e) {
     e.preventDefault();
 
     $(this).closest('.popover-account').removeClass('active');
