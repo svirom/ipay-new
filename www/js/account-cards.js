a66a1924927fbf2b387dc0fba2e846e07f4900e5
svirom <svirom@yahoo.com>
2020-07-20 16:33:52 +0300

    my cards

---
+++ www/js/account-cards.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  $('.account-card').on('click', function(e) {
+    var docWidth = $(document).width();
+    e.preventDefault();
+    console.log(e.target);
+
+    if ( $(e.target).hasClass('account-card__confirm-cancel') ) {
+      $(this).find('.account-card__confirm').fadeOut(400);
+      $(document).find('body').removeClass('overflow-hidden');
+    }
+
+    if ( $(e.target).hasClass('account-card__mobile-cancel') ) {
+      $(this).find('.account-card__mobile').fadeOut(400);
+      $(document).find('body').removeClass('overflow-hidden');
+    }
+
+    if ( $(e.target).hasClass('account-card__delete-button') ) {
+      if ( docWidth >= 576 ) {
+        $(this).find('.account-card__confirm').fadeIn(400);
+      } else {
+        $(this).find('.account-card__mobile').fadeIn(400);
+        $(document).find('body').addClass('overflow-hidden');
+      }
+    }
+
+
+
+  })
+
+})
