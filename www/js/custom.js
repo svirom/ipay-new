be6e7d2924a7613facae1ec15de038f5b53abc17
svirom <svirom@yahoo.com>
2020-07-30 17:03:59 +0300

    sign in

--- www/js/custom.js
+++ www/js/custom.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 
   function togglePassword() {
     var inp = $(this).closest('.form-group').find('.form-input');
+    
     if (inp.attr('type') == "password") {
         inp.attr('type', 'text');
         $(this).addClass('show-pass');
