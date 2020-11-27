a8090fd76db8e446404b3024005d04b581adf7d5
svirom <svirom@yahoo.com>
2020-11-27 14:36:26 +0200

    p2p

--- www/js/p2p-main.js
+++ www/js/p2p-main.js
@@ -8,7 +8,16 @@ $(document).ready(function() {
     html: true,
     template: '<div class="popover popover-bonuses" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
   }
+
+  // invoice popover
+  invoiceOptions = {
+    content: 'Вы можете посчитать сумму перевода прямо в поле. Например, 700+450-150',
+    trigger: 'toggle',
+    html: true,
+    template: '<div class="popover popover-bonuses" role="tooltip"><div class="arrow"></div><div class="popover-body py-3"></div></div>'
+  }
   
   $("#bonus-tooltip").popover(bonusesOptions);
+  $("#invoice-popover").popover(invoiceOptions);
 
 })
