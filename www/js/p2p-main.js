5166c1639fc5d9ffe1d0e4c26e70e9a8fc7dce2a
svirom <svirom@yahoo.com>
2020-11-27 16:04:32 +0200

    p2p

--- www/js/p2p-main.js
+++ www/js/p2p-main.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
 
+  var docWidth = $(document).width();
+
   // bonuses popover
   bonusesOptions = {
     title: 'Что такое Бонусы Добра?',
     content: 'Это бонусы, которые Вы накапливаете в личном кабинете на сервисе iPay.ua при совершении платежей или переводе денег с карты на карту.<br><br> <a href="https://ipay-v4.romaniuk.office.udc.ua/ru/yapomoga" target="_blank">Подробнее</a>',
     trigger: 'toggle',
+    placement: 'right',
     html: true,
     template: '<div class="popover popover-bonuses" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
   }
@@ -13,8 +16,14 @@ $(document).ready(function() {
   invoiceOptions = {
     content: 'Вы можете посчитать сумму перевода прямо в поле. Например, 700+450-150',
     trigger: 'toggle',
+    placement: 'right',
     html: true,
     template: '<div class="popover popover-bonuses" role="tooltip"><div class="arrow"></div><div class="popover-body py-3"></div></div>'
+  }
+
+  if (docWidth < 768) {
+    bonusesOptions.placement = 'bottom';
+    invoiceOptions.placement = 'bottom';
   }
   
   $("#bonus-tooltip").popover(bonusesOptions);
