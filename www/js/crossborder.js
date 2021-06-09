a8dea78e980425abd408857e5e638efa919a7bfc
svirom <svirom@yahoo.com>
2021-06-09 20:04:21 +0300

    crossborder

---
+++ www/js/crossborder.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+  // change currency on radio checkbox
+  $('.crossborder__transfer-currency input[type=radio]').change(function() {
+    if ($(this).val() == 'eur') {
+      $(this).parents('.crossborder__transfer-currency')
+        .find('.label-currency').text('EUR').end()
+        .find('.currency-eur').addClass('active').end()
+        .find('.currency-pln').removeClass('active');
+    }
+    else if ($(this).val() == 'pln') {
+      $(this).parents('.crossborder__transfer-currency')
+        .find('.label-currency').text('PLN').end()
+        .find('.currency-pln').addClass('active').end()
+        .find('.currency-eur').removeClass('active');;
+    }
+  });
+
+  var docWidth = $(document).width();
+
+  // crossborder popovers
+	crossborderOptions = {
+		trigger: 'toggle',
+		placement: 'right',
+		html: true,
+		template: '<div class="popover popover-crossborder" role="tooltip"><div class="arrow"></div><div class="popover-body"></div></div>'
+	}
+
+  if (docWidth < 768) {
+    bonusesOptions.placement = 'bottom';
+  }
+
+  $(".crossborder-tooltip").popover(crossborderOptions);
+
+  
+
+})
