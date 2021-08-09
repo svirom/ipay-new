7955669a73c21a950296883cc17221fb466a5046
svirom <svirom@yahoo.com>
2021-08-09 21:06:56 +0300

    homepage new tiles

--- www/js/select2-style.js
+++ www/js/select2-style.js
@@ -19,4 +19,31 @@ $(document).ready(function() {
     dropdownParent: $('.select-input-wrapper')
   });
 
+  $('.select-search').select2({
+    placeholder: "Область",
+    allowClear: true,
+    dropdownCssClass: 'select-dropdown',
+    selectionCssClass: 'select-selection',
+    minimumResultsForSearch: 3,
+    dropdownParent: $('.select-input-wrapper'),
+    width: '100%'
+  });
+
+  $(".select-input-wrapper").each(function () {
+    $(this).find('.js-example-basic-single').select2({
+        placeholder: $(this).data('label'),
+        allowClear: true,
+        dropdownCssClass: 'select-dropdown',
+        selectionCssClass: 'select-selection',
+        minimumResultsForSearch: $(this).find('.js-example-basic-single').hasClass('select-search') ? 3 : Infinity,
+        dropdownParent: $(this),
+        width: '100%',
+        language: {
+          noResults: function() {
+            return "Ничего не найдено";
+          }
+        }
+    });
+});
+
 })
