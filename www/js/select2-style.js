b9104d0da420461d977fe9e4d2676513320084bd
svirom <svirom@yahoo.com>
2021-09-22 18:43:37 +0300

    bill eps

--- www/js/select2-style.js
+++ www/js/select2-style.js
@@ -32,19 +32,46 @@ $(document).ready(function() {
   });
 
   $(".select-input-wrapper").each(function () {
+
+    var lang = $('html').attr('lang');
+    var searchPlaceholder = '';
+    var searchNothing = '';
+
+    switch (lang) {
+      case 'ru-RU':
+        searchPlaceholder = 'Начните вводить...';
+        searchNothing = 'Ничего не найдено';
+        break;
+      case 'uk-UA':
+        searchPlaceholder = 'Почніть вводити...';
+        searchNothing = 'Нічого не знайдено';
+        break;
+      case 'en-US':
+        searchPlaceholder = 'Start typing...';
+        searchNothing = 'Nothing found';
+        break;
+      default:
+        searchPlaceholder = 'Начните вводить...';
+        searchNothing = 'Ничего не найдено';
+    }
+
     $(this).find('.js-example-basic-single').select2({
-        placeholder: $(this).data('label'),
-        allowClear: true,
-        dropdownCssClass: 'select-dropdown',
-        selectionCssClass: 'select-selection',
-        minimumResultsForSearch: $(this).find('.js-example-basic-single').hasClass('select-search') ? 3 : Infinity,
-        dropdownParent: $(this),
-        width: '100%',
-        language: {
-          noResults: function() {
-            return "Ничего не найдено";
-          }
+      placeholder: $(this).data('label'),
+      allowClear: true,
+      dropdownCssClass: 'select-dropdown',
+      selectionCssClass: 'select-selection',
+      minimumResultsForSearch: $(this).find('.js-example-basic-single').hasClass('select-search') ? 3 : Infinity,
+      dropdownParent: $(this),
+      width: '100%',
+      language: {
+        noResults: function() {
+          return searchNothing;
         }
+      }
+    });
+
+    $(this).one('select2:open', function(e) {
+      $('input.select2-search__field').prop('placeholder', searchPlaceholder);
     });
   });
 
