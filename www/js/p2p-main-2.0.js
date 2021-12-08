74232149312b0d6f5d7b9bd8a11044c945250cd5
svirom <svirom@yahoo.com>
2021-12-08 19:23:49 +0200

    p2p new page start

---
+++ www/js/p2p-main-2.0.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+  var docWidth = $(document).width();
+
+  // bonuses popover
+  bonusesOptions = {
+    // title: 'Что такое Бонусы Добра?',
+    // content: 'Это бонусы, которые Вы накапливаете в личном кабинете на сервисе iPay.ua при совершении платежей или переводе денег с карты на карту.<br><br> <a href="https://ipay-v4.romaniuk.office.udc.ua/ru/yapomoga" target="_blank">Подробнее</a>',
+    trigger: 'toggle',
+    placement: 'right',
+    html: true,
+    template: '<div class="popover popover-bonuses" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+  }
+
+  // invoice popover
+  invoiceOptions = {
+    // content: 'Вы можете посчитать сумму перевода прямо в поле. Например, 700+450-150',
+    trigger: 'toggle',
+    // placement: 'right',
+    html: true,
+    template: '<div class="popover popover-bonuses" role="tooltip"><div class="arrow"></div><div class="popover-body py-3"></div></div>'
+  }
+
+  // p2p connection
+	popoverOptions = {
+		trigger: 'toggle',
+		// trigger: 'click',
+		// placement: 'right',
+		html: true,
+		template: '<div class="popover popover-bonuses p2p-popover" role="tooltip"><div class="arrow"></div><div class="popover-body py-3"></div></div>'
+	}
+
+  if (docWidth < 768) {
+    bonusesOptions.placement = 'bottom';
+    invoiceOptions.placement = 'bottom';
+    popoverOptions.placement = 'bottom';
+  }
+  
+  $("#bonus-tooltip").popover(bonusesOptions);
+  $("#invoice-popover").popover(invoiceOptions);
+  $(".p2p-popover").popover(popoverOptions);
+
+  // p2p masterpass always one collapsed element open
+  $('.payment-method--p2p [data-toggle=collapse]').on('click', function (e) {
+    e.preventDefault();
+    if(!$(this).hasClass('collapsed')){
+      e.stopPropagation();
+      return false;
+    }
+  })
+
+})
