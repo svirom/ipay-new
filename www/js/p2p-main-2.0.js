b35b3c0d3336afe9c66616cf6319d813f9e4b129
svirom <svirom@yahoo.com>
2021-12-11 17:17:25 +0200

    p2p new page

--- www/js/p2p-main-2.0.js
+++ www/js/p2p-main-2.0.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
 
   // bonuses popover
   bonusesOptions = {
-    // title: 'Что такое Бонусы Добра?',
-    // content: 'Это бонусы, которые Вы накапливаете в личном кабинете на сервисе iPay.ua при совершении платежей или переводе денег с карты на карту.<br><br> <a href="https://ipay-v4.romaniuk.office.udc.ua/ru/yapomoga" target="_blank">Подробнее</a>',
     trigger: 'toggle',
     placement: 'right',
     html: true,
@@ -14,9 +12,7 @@ $(document).ready(function() {
 
   // invoice popover
   invoiceOptions = {
-    // content: 'Вы можете посчитать сумму перевода прямо в поле. Например, 700+450-150',
     trigger: 'toggle',
-    // placement: 'right',
     html: true,
     template: '<div class="popover popover-bonuses" role="tooltip"><div class="arrow"></div><div class="popover-body py-3"></div></div>'
   }
@@ -24,8 +20,6 @@ $(document).ready(function() {
   // p2p connection
 	popoverOptions = {
 		trigger: 'toggle',
-		// trigger: 'click',
-		// placement: 'right',
 		html: true,
 		template: '<div class="popover popover-bonuses p2p-popover" role="tooltip"><div class="arrow"></div><div class="popover-body py-3"></div></div>'
 	}
@@ -40,13 +34,54 @@ $(document).ready(function() {
   $("#invoice-popover").popover(invoiceOptions);
   $(".p2p-popover").popover(popoverOptions);
 
-  // p2p masterpass always one collapsed element open
-  $('.payment-method--p2p [data-toggle=collapse]').on('click', function (e) {
-    e.preventDefault();
-    if(!$(this).hasClass('collapsed')){
-      e.stopPropagation();
-      return false;
+  // p2p masterpass
+  $('.p2p-tablist__link').on('shown.bs.tab', function (event) {
+    var senderTitle = $(event.target).data('title');
+
+    $('#p2p-sender-title').text(senderTitle);
+
+    if ($('#p2p-masterpass').is(':visible')) {
+      // console.log('visible');
     }
+  })
+
+  var activeCard = $('#sender-mask-card-active').text();
+
+  // choose masterpass card
+  $('#masterpass-cards .masterpass-card').each(function () {
+    var card = $(this).find('.masterpass-card__card').text();
+
+    if (activeCard == card) {
+      $(this).addClass('masterpass-card--active');
+    }
+
+    $(this).on('click', function () {
+      var text = $(this).find('.masterpass-card__purpose').text();
+      var card = $(this).find('.masterpass-card__card').text();
+      var bank = $(this).find('.mp-payment-card').attr('class');
+      // var cardAlias = $(this).find('.masterpass-card__alias').val();
+      var bankLogo = bank.slice(bank.lastIndexOf(' '));
+      var type = $(this).find('.masterpass-card__type').attr('class');
+      var cardType = type.slice(type.lastIndexOf(' '));
+
+      $('#sender-title-card-active').text(text);
+      $('#sender-mask-card-active').text(card);
+      // $('#p2pform-sender_masterpass_card_alias').val(cardAlias);
+
+      $('#sender-bank-card-active').removeClass();
+      $('#sender-bank-card-active').addClass('masterpass-card__bank mp-payment-card ' + bankLogo);
+
+      $('#sender-type-card-active').removeClass();
+      $('#sender-type-card-active').addClass('masterpass-card__type ' + cardType);
+
+      $(".dropdown-menu .masterpass-card--active").removeClass('masterpass-card--active');
+      $(this).addClass('masterpass-card--active');
+
+      // happy addressee check on masterpass
+      // masterPassSmsCheck($('#sender-mask-card-active').text()[0]);
+
+      // calcCommission($('#sender-mask-card-active'));
+    })
   })
 
 })
