36ab3fb2f40a5403774c99a78f044b1a53b382d2
svirom <svirom@yahoo.com>
2024-06-26 22:51:13 +0300

    account new templates

--- www/js/account-cards.js
+++ www/js/account-cards.js
@@ -35,4 +35,27 @@ $(document).ready(function() {
 
   })
 
+  function checkPan(elem) {
+    var cardMask = $(elem).find('.account-card__card').data('mask')
+    var ips = cardMask.substr(cardMask.length * (-1), 1);
+
+    switch (ips) {
+      case '4':
+        $(elem).find('.card-title-mark').addClass('type-card-visa')
+        break;
+      case '5':
+        $(elem).find('.card-title-mark').addClass('type-card-mc')
+        break;
+      case '6':
+        $(elem).find('.card-title-mark').addClass('type-card-cirus')
+        break;
+      case '9':
+        $(elem).find('.card-title-mark').addClass('type-card-prostir')
+        break;
+    }
+  }
+  $(".account-card").each(function (){
+    checkPan($(this));
+  })
+
 })
