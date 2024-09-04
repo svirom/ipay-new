c28ea53671e31f5c502775304cb0fe570616da0b
svirom <svirom@yahoo.com>
2024-09-04 15:41:53 +0300

    mastercard click to pay

--- www/js/api-mastercard-old.js
+++ www/js/api-mastercard-old.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   
   var params = {
+    // dpaId: "6bc8ee6a-ebd5-417c-8117-377ece9bce77", // required DPA Identifier, generated during registration.
     srcDpaId: "6bc8ee6a-Ebd5-417c-8117-377ece9bce77_dpa0", // required DPA Identifier, generated during registration.
     dpaData: {
      dpaName: "Testdpa0"       //required
@@ -12,9 +13,32 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   var mcCheckoutService = new MastercardCheckoutServices()
 
+  // let checkoutInit = async function() {
+  //   try {
+  //    var result = await mcCheckoutService.init(params);    
+  //    // handle result: an object containing a list of initialized card networks
+  //    // upon successful resolution, other methods, e.g. getCards(), may be called
+  //    console.log(result);
+    
+
+  //   } 
+  //   //  catch (error) {
+  //    catch ({name, message}) {
+  //     // handle error
+  //     // console.log(error);
+  //     console.log(name);
+  //     console.log(message);
+  //    }
+  // }
+
+  // checkoutInit();
+
   const initPromise = mcCheckoutService.init(params) //  returns a promise
 
+  // console.log('initPromise', initPromise);
+
   initPromise
+    // .then(promiseResolvedHandler)  // No other library methods should be invoked until `init` resolves
     .then(function(initResult) {
       console.log('initResult', initResult);
       const getCardsPromise = mcCheckoutService.getCards();
@@ -25,22 +49,14 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log(`Cards: ${cardsArray}`);
 
       if (!cardsArray.length) {
-       
-        // idLookupHandler();
-        const sampleiDLookupParams = {"email": 'svia.rom@gmail.com'};
-     
-        const promiseResolvedPayload = mcCheckoutService.idLookup(sampleiDLookupParams);
-        console.log('hello', promiseResolvedPayload);
-        return promiseResolvedPayload;
+        const sampleiDLookupParams = 'svia.rom@gmail.com';
+        // const idLookupPromise = mcCheckoutService.idLookup(sampleiDLookupParams);
+        // return idLookupPromise;
+        // console.log(idLookupPromise);
       }
     })
     .then(function(isIdLookup) {
-      console.log(isIdLookup);
-      if (isIdLookup.consumerPresent) {
-        console.log(isIdLookup);
-      } else {
-        console.log('false');
-      }
+      // console.log(isIdLookup); 
     })
     .catch(promiseRejectedHandler)
   
@@ -74,5 +90,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // })
 
+  async function idLookupHandler () { // this method will return a promise
+    try {
+      const sampleiDLookupParams = 'svia.rom@gmail.com';
+      const promiseResolvedPayload = await mcCheckoutService.idLookup(sampleiDLookupParams);
+      console.log('idLookup: ' + promiseResolvedPayload);
+      // add success handler logic here
+      // or
+      // promiseResolvedHandler(promiseResolvedPayload)
+    } catch (error) {
+      // console.log(error);
+     
+      // add error handler logic here
+      // or
+      // promiseRejectedHandler(promiseRejectedPayload)
+    }
+  }
+
+  idLookupHandler();
 
 });
