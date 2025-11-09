98941bb194f1496f8349a856d9e985698f30e48b
svirom <svirom@yahoo.com>
2025-11-10 01:11:47 +0200

    promocodes page

--- www/js/promocodes.js
+++ www/js/promocodes.js
@@ -30,19 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
     slideMargin: 30,
     easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
     speed: 600,
-    // controls: false,
-		// pager: false,
     pause: 4000,
-    // responsive : [
-    //   {
-    //     breakpoint: 991,
-    //     settings: {
-    //       item: 1,
-    //       // slideMove: 1,
-    //       slideMargin: 30,
-    //     }
-    //   }
-    // ],
 
     onSliderLoad: function (el) {
       heightMeasurement();
@@ -85,14 +73,14 @@ document.addEventListener('DOMContentLoaded', function() {
   modalShowButtons.forEach(function(button) {
     button.addEventListener('click', function() {
       const activeCard = button.closest('.promocodes-card');
-      const cardImg = activeCard.querySelector('.promocodes-card__img > img');
+      const cardImg = activeCard.querySelector('.promocodes-card__img img');
       const cardTitle = activeCard.querySelector('.promocodes-card__title');
       const cardDate = activeCard.querySelector('.promocodes-card__date');
       const cardInfo = activeCard.querySelector('.promocodes-card__info');
       const cardUrl = activeCard.dataset.cardUrl;
 
       const cardModal = document.getElementById('promocodes-card-modal');
-      let modalImg = cardModal.querySelector('.promocodes-card-modal__img > img');
+      let modalImg = cardModal.querySelector('.promocodes-card-modal__img img');
       let modalTitle = cardModal.querySelector('.promocodes-card-modal__title');
       let modalInfo = cardModal.querySelector('.promocodes-card-modal__info');
       let modalUrl = cardModal.querySelector('.promocodes-card-modal__url');
@@ -105,7 +93,65 @@ document.addEventListener('DOMContentLoaded', function() {
       modalDate.textContent = cardDate.textContent.trim();
 
       cardDate.classList.contains('promocodes-card__date--hot') ? modalDate.classList.add('promocodes-card-modal__date--hot') : modalDate.classList.remove('promocodes-card-modal__date--hot');
+
+      if (activeCard.dataset.cardPromocode) {
+        const div = document.createElement('div');
+        const span = document.createElement('span');
+        const a = document.createElement('a');
+
+        div.classList.add('promocodes-card-modal__promocode');
+        span.textContent = activeCard.dataset.cardPromocode;
+        a.classList.add('promocodes-card-modal__promocode--copy');
+        div.append(span, a);
+
+        modalInfo.parentNode.insertBefore(div, modalInfo.nextSibling);
+
+      } else {
+        if (document.querySelector('.promocodes-card-modal__promocode')) {
+          document.querySelector('.promocodes-card-modal__promocode').remove();
+        }
+      }
     })
+  });
+
+  $('#promocodes-card-modal').on('hidden.bs.modal', function() {
+    const cardModal = document.getElementById('promocodes-card-modal');
+    let modalImg = cardModal.querySelector('.promocodes-card-modal__img img');
+    let modalTitle = cardModal.querySelector('.promocodes-card-modal__title');
+    let modalInfo = cardModal.querySelector('.promocodes-card-modal__info');
+    let modalUrl = cardModal.querySelector('.promocodes-card-modal__url');
+    let modalDate = cardModal.querySelector('.promocodes-card-modal__date');
+
+    modalImg.src = '';
+    modalTitle.textContent = '';
+    modalInfo.textContent = '';
+    modalUrl.href = '';
+    modalDate.textContent = '';
+
+    modalDate.classList.remove('promocodes-card-modal__date--hot');
+
+    if (document.querySelector('.promocodes-card-modal__promocode')) {
+      document.querySelector('.promocodes-card-modal__promocode').remove();
+    }
+  })
+
+  document.querySelector('.modal-body').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains('promocodes-card-modal__promocode--copy')) {
+      console.log('good');
+      const promocode = document.querySelector('.promocodes-card-modal__promocode > span').textContent; 
+      if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+        document.querySelector('.promocodes-card-modal__promocode').classList.add('copied');
+
+        setTimeout(() => {
+          document.querySelector('.promocodes-card-modal__promocode').classList.remove('copied');
+        }, 1000);
+
+        return navigator.clipboard.writeText(promocode);
+      }
+      return Promise.reject('The Clipboard API is not available.');
+    }
   })
 
 })
